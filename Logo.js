64f6d9b915a3251b47d81db7e91272bde099c56c
stablesmp <kingen.elias2012@gmail.com>
2026-06-06 13:01:40 +0200

    Update Logo.js

--- Logo.js
+++ Logo.js
@@ -14,7 +14,7 @@ export const Logo = ({ className = "", size = "md" }) => {
   return html`
     <div className=${`flex items-center font-black tracking-tighter select-none group ${sizeClasses[size]} ${className}`}>
       <span className="relative inline-block transition-transform duration-500 group-hover:scale-105">
-        <span className="relative text-green-glow transition-all duration-500">Stable</span>
+        <span className="relative text-green-glow transition-all duration-500">Headbound</span>
       </span>
       <span className="text-gray-900 transition-transform duration-500 group-hover:scale-105 inline-block">SMP</span>
     </div>
